d3e39bb24309775a09f79196634b4d810c35ee31
Jorge <jorgefdt@gmail.com>
2019-05-03 19:42:48 -0400

    Refactored some and added function to calculate LabelNames available in file train_annotations-bbox-100.csv (which I am using capped during dev because of the large size)

--- src/openimager/openimager.py
+++ src/openimager/openimager.py
@@ -13,29 +13,131 @@ from checkpoints import checkpoints
 checkpoints.enable()
 
 
+##
+## -- UTILITIES
+##
+
+def _startPrintDataFrame(title):
+    print(f"{title}", end='')
+
+
+def _endPrintDataFrame(df, verbose=False, index=True):
+    print(f" ({df.shape[0]})")
+    if verbose:
+        print(f"{df if index else df.to_string(index=False)}")
+        print()
+
+
+def _printDataFrame(title, df, verbose=False, index=True):
+    _startPrintDataFrame(title)
+    _endPrintDataFrame(df, verbose, index)
+        
+
+##
+## -- DATA LOADERS
+##
+
+def _downloadClassNames(class_names_fp=None, verbose=False):
+    """Download or load the class names pandas DataFrame"""
+    _startPrintDataFrame(":: Loading class_names")
+    kwargs = {'header': None, 'names': ['LabelID', 'LabelName']}
+    # orig_url = "https://storage.googleapis.com/openimages/2018_04/class-descriptions-boxable.csv"
+    orig_url = "../../data/class-descriptions-boxable.csv"
+    retValue = pd.read_csv(class_names_fp, **kwargs) if class_names_fp else pd.read_csv(orig_url, **kwargs)
+    _endPrintDataFrame(retValue, verbose=True)
+    return retValue
+
+
+def _downloadTrainedBoxed(train_boxed_fp=None, verbose=False):
+    """Download or load the boxed image metadata pandas DataFrame"""
+    _startPrintDataFrame(":: Loading train_boxed")
+    # TODO: setting index_col should not be necessary in this and the next section, update the save-to-disk code
+    # orig_url = "https://storage.googleapis.com/openimages/2018_04/train/train-annotations-bbox.csv"
+    # orig_url = "../../data/train-annotations-bbox.csv"
+    orig_url = "../../data/train-annotations-bbox-100.csv"
+    retValue = pd.read_csv(train_boxed_fp, index_col=0) if train_boxed_fp else pd.read_csv(orig_url)
+    _endPrintDataFrame(retValue, verbose)
+    return retValue
+
+
+def _getImageIds(image_ids_fp=None, verbose=False):
+    """Download or load the image ids metadata pandas DataFrame"""
+    _startPrintDataFrame(":: Loading image_ids")
+    # orig_url = "https://storage.googleapis.com/openimages/2018_04/train/train-images-boxable-with-rotation.csv"
+    orig_url = "../../data/train-images-boxable-with-rotation.csv"
+    retValue = pd.read_csv(image_ids_fp, index_col=0) if image_ids_fp else pd.read_csv(orig_url)
+    _endPrintDataFrame(retValue, verbose)
+    return retValue
+
+
+##
+## -- IO UTILS
+##
+@ratelim.patient(5, 5)
+def _download_image(url, pbar):
+    """Download a single image from a URL, rate-limited to once per second"""
+    r = requests.get(url)
+    r.raise_for_status()
+    pbar.update(1)
+    return r
+
+
+def _write_image_file(r, image_name):
+    """Write an image to a file"""
+    filename = f"temp/{image_name}"
+    with open(filename, "wb") as f:
+        f.write(r.content)
+
+
+##
+## -- HELPERS
+##
+
+def _find_labels_used_in_train_boxed():
+    """Calculates the labelNames used by the train_boxed data file."""
+    class_names = _downloadClassNames()
+    train_boxed = _downloadTrainedBoxed()
+
+    # Get the labelIDs used in train_boxed
+    print(f":: Calculating usedLabelIDs", end='')
+    usedLabelIDs = train_boxed[['LabelName']]
+    usedLabelIDs = usedLabelIDs.drop_duplicates() \
+                        .sort_values("LabelName") \
+                        .reset_index() \
+                        .drop(columns=['index']) \
+                        .rename(columns={'LabelName':'LabelID'})
+    _endPrintDataFrame(usedLabelIDs, verbose=False)
+
+    # Find labels 
+    print(f":: Mapping used label IDs to label names", end='')
+    usedLabels = class_names.merge(usedLabelIDs).LabelName.sort_values()
+    _endPrintDataFrame(usedLabels, verbose=True, index=False)
+
+
+
+##
+## -- MAIN
+##
+
 def download(categories,  # packagename, registry,
              class_names_fp=None, train_boxed_fp=None, image_ids_fp=None):
     """Download images in categories from flickr"""
+    print(f":: Download on categories: {categories}")
 
-    # Download or load the class names pandas DataFrame
-    kwargs = {'header': None, 'names': ['LabelID', 'LabelName']}
-    orig_url = "https://storage.googleapis.com/openimages/2018_04/class-descriptions-boxable.csv"
-    class_names = pd.read_csv(class_names_fp, **kwargs) if class_names_fp else pd.read_csv(orig_url, **kwargs)
-
-    # TODO: setting index_col should not be necessary in this and the next section, update the save-to-disk code
-    # Download or load the boxed image metadata pandas DataFrame
-    orig_url = "https://storage.googleapis.com/openimages/2018_04/train/train-annotations-bbox.csv"
-    train_boxed = pd.read_csv(train_boxed_fp, index_col=0) if train_boxed_fp else pd.read_csv(orig_url)
-
-    # Download or load the image ids metadata pandas DataFrame
-    orig_url = "https://storage.googleapis.com/openimages/2018_04/train/train-images-boxable-with-rotation.csv"
-    image_ids = pd.read_csv(image_ids_fp, index_col=0) if image_ids_fp else pd.read_csv(orig_url)
+    class_names = _downloadClassNames(class_names_fp)
+    train_boxed = _downloadTrainedBoxed(train_boxed_fp)
+    image_ids = _getImageIds(image_ids_fp)
 
     # Get category IDs for the given categories and sub-select train_boxed with them.
+    print(f":: Mapping labels")
     label_map = dict(class_names.set_index('LabelName').loc[categories, 'LabelID']
                      .to_frame().reset_index().set_index('LabelID')['LabelName'])
+    print(f":: label_map: {label_map}")
     label_values = set(label_map.keys())
+    print(f":: label_values: {label_values}")
     relevant_training_images = train_boxed[train_boxed.LabelName.isin(label_values)]
+    print(f":: relevant_training_images: {relevant_training_images}")
+
 
     # Start from prior results if they exist and are specified, otherwise start from scratch.
     relevant_flickr_urls = (relevant_training_images.set_index('ImageID')
@@ -68,22 +170,8 @@ def download(categories,  # packagename, registry,
         _write_image_file(r, image_name)
 
 
-@ratelim.patient(5, 5)
-def _download_image(url, pbar):
-    """Download a single image from a URL, rate-limited to once per second"""
-    r = requests.get(url)
-    r.raise_for_status()
-    pbar.update(1)
-    return r
-
-
-def _write_image_file(r, image_name):
-    """Write an image to a file"""
-    filename = f"temp/{image_name}"
-    with open(filename, "wb") as f:
-        f.write(r.content)
-
-
 if __name__ == '__main__':
     categories = sys.argv[1:]
-    download(categories)
+    # download(categories)
+    _find_labels_used_in_train_boxed()
+    print(f":: Done")
